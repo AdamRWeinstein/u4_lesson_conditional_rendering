a3582994ac9bf3d73d189733128217b3e7dceca7
AdamRWeinstein <adam.rick.weinstein@gmail.com>
2024-01-29 09:13:58 -0500

    Finish lesson

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,12 @@
+import Welcome from "./Welcome"
+import Denied from "./Denied"
+
+const Greeting = ({ loggedIn }) => {
+    if (loggedIn) {
+        return <Welcome />
+    } else {
+        return <Denied />
+    }
+}
+
+export default Greeting
